783a96c98958b35dc1875e25bf55d75439cd03e9
Vidya <vidya6988@gmail.com>
2014-04-10 16:58:20 +0200

    File structure modified

---
+++ LearnRiak/View Controllers/LRSplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  LRSplashViewController.h
+//  LearnRiak
+//
+//  Created by Vidya on 10/04/14.
+//  Copyright (c) 2014 Vidya Murthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRSplashViewController : UIViewController
+
+@end
---
+++ LearnRiak/View Controllers/LRSplashViewController.m
@@ -0,0 +1,38 @@
+//
+//  LRSplashViewController.m
+//  LearnRiak
+//
+//  Created by Vidya on 10/04/14.
+//  Copyright (c) 2014 Vidya Murthy. All rights reserved.
+//
+
+#import "LRSplashViewController.h"
+
+@interface LRSplashViewController ()
+
+@end
+
+@implementation LRSplashViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
