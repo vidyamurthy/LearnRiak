e6be3126243013dfde1423047b5d1aba19ff1795
Vidya <vidya6988@gmail.com>
2014-04-10 16:50:42 +0200

    First Commit

---
+++ LearnRiak/LRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LRAppDelegate.h
+//  LearnRiak
+//
+//  Created by Vidya on 10/04/14.
+//  Copyright (c) 2014 Vidya Murthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LearnRiak/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearnRiak
+//
+//  Created by Vidya on 10/04/14.
+//  Copyright (c) 2014 Vidya Murthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LRAppDelegate class]));
+    }
+}
